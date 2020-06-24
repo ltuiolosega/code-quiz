13c300f0a582884293f88a74957c3230b29a2748
ltuiolosega <l.tuiolosega59@gmail.com>
2020-06-23 23:40:01 -0700

    updated all files and readme

--- quiz-script.js
+++ quiz-script.js
@@ -45,3 +45,15 @@ function loadNextQuestion () {
     loadQuestion(currentQuestion);
 }
 loadQuestion(currentQuestion);
+
+var sec = 15;
+var time = setInterval(myTimer, 1000);
+
+function myTimer() {
+    document.getElementById('timer').innerHTML = sec + "sec left";
+    sec--;
+    if (sec == -1) {
+        clearInterval(time);
+        alert("Time out!! :(");
+    }
+}
